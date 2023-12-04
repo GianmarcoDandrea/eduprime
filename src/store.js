c78678453dd865069a2e349aea3bb937b870028d
GianmarcoDandrea <gianm96@hotmail.it>
2023-12-04 17:34:40 +0100

    add: store.js

---
+++ src/store.js
@@ -0,0 +1,14 @@
+import {reactive} from "vue";
+
+export const store = reactive({
+    
+    headerLinkList: [
+        "Home",
+        "Courses",
+        "About Us",
+        "News",
+        "Pages",
+        "Contact",
+        "Purchase"
+    ]
+})
