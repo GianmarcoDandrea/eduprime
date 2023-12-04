96782b05f1560ed9f3f945ace8993551acfd6255
GianmarcoDandrea <gianm96@hotmail.it>
2023-12-04 17:07:53 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import '@fontsource/fredoka-one';
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+  components : {AppFooter, AppHeader, AppMain}
+}
+
+</script>
+
+<template>
+  <AppHeader />
+  <AppMain />
+  <AppFooter />
+
+</template>
+
+<style lang="scss">
+@use "./style/general.scss" as *;
+@use "./style/partials/variables" as *;
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+            
+}
+</script>
+<template >
+
+</template>
+<style lang="scss" scoped>
+@use "../style/partials/variables";
+@use "../style/partials/mixins.scss";
+    
+</style>
